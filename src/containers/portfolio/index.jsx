421a210dca7dbec5d29f8d4cf749fb48c69f26c0
Sweta Kumari Mishra <mishrasweta1001@gmail.com>
2023-12-29 01:45:21 +0530

    Portfolio section completed

--- src/containers/portfolio/index.jsx
+++ src/containers/portfolio/index.jsx
@@ -2,17 +2,85 @@ import React from "react";
 // import { AiTwotoneProject  } from "react-icons/ai";
 import { BiAlignJustify } from "react-icons/bi";
 import PageHeaderContent from "../../components/pageHeaderContent";
+import {Animate} from 'react-simple-animate';
+import './style.scss';
 
-const Portfolio = () =>
-{
-  return(
+
+const project = [
+  {
+    title: "React responsive gym website",
+    description: "In this Reactjs project, we make an awesome responsive gym website step-by-step. We use React hooks, modern CSS, framer motion,and many more. For contact, we will use the Email js library to make this site more functional and practical",
+    button: "visit",
+    link : "https://github.com/SwetaKumariMishra/Fit-Club-using-ReactJS",
+  },
+  {
+    title: "DSA and Problem solving using Java",
+    description: "In this section, Data structures in Java are a group of data elements through which data are stored and organized in the computer so they can be used more efficiently",
+    button: "visit",
+    link: "https://github.com/SwetaKumariMishra/Problem-Solving-in-Java",
+  },
+  {
+    title: "Google Note Clone using JavaScript",
+    description: "In this JavaScript project, we create Google Keep App Clone with Local Storage using JavaScript",
+    button: "visit",
+    link: "https://github.com/SwetaKumariMishra/Projects-using-JavaScript/tree/master/GoogleNoteClone",
+  },
+  {
+    title: "Temperature converter using JavaScript",
+    description: "In this JavaScript project, we create an input element that can convert a value from one temperature measurement to another.",
+    button: "visit",
+    link: "https://github.com/SwetaKumariMishra/Projects-using-JavaScript/tree/master/TempConverter",
+  },
+  {
+    title: "Incerement Counter",
+    description: "In this JavaScript project, we build an increment counter on button click",
+    button: "visit",
+    link: "https://github.com/SwetaKumariMishra/Projects-using-JavaScript/tree/master/IncrementCounter",
+  },
+]
+
+const Portfolio = () => {
+  return (
     <section id="portfolio" className="portfolio">
       <PageHeaderContent
-        headerText = "My Portfolio"
-        icon = {< BiAlignJustify size={40} />}
+        headerText="My Portfolio"
+        icon={<BiAlignJustify size={40} />}
       />
+      <Animate
+              play
+              duration={1}
+              delay={0.3}
+              start={{
+                transform: "translateX(-200px)",
+              }}
+              end={{
+                transform: "translateX(0px)",
+              }}
+            >
+         <div className="container">
+         <h1 className="main">My Projects</h1>
+      <div className="card-container">
+        <div className="cards">
+          {
+            project.map((item, i)=>
+            (
+              <div className="card" key={i}>
+              <h1>{item.title}</h1>
+              <p>{item.description}</p>
+              <a href={item.link} target="_blank" rel="noopener noreferrer" className="card-button">{item.button}</a>
+            
+              </div>
+              
+            ))
+          }
+          
+        </div>
+      
+      </div>
+      </div>
+      </Animate>
     </section>
   );
-}
+};
 
 export default Portfolio;
